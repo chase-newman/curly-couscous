6093286d26d34b94804decae4226d01a04d8575c
Chase Newman <chasejn01@gmail.com>
2022-04-10 12:26:37 -0400

    add database connection

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 console.log("Connected");
 
+
+
+fetch("https://test-project-d14cc-default-rtdb.firebaseio.com/new-post.json")
+    .then(response => response.json())
+    .then(data => {
+        
+        const key = Object.keys(data)[0];
+        const stockPrices = Object.values(data)
+        console.log(stockPrices)
+        
+        // stockPrices.forEach((el) => {
+        //   console.log(el); 
+        // });
+        
+        for(let price of stockPrices) {
+            console.log(price);
+        }
+        
+    }).catch(e => console.log(e))
